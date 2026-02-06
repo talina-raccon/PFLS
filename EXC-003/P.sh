81648e1edf9d8b7860f0b9e8194b54ac85f32638
Talina Helms <talina.helms@ck-helms.de>
2026-02-06 14:41:31 +0100

    my changes

---
+++ EXC-003/P.sh
@@ -0,0 +1,20 @@
+for f in $@
+do 
+    echo "FASTA File_Statistic" # Header 
+    echo "----------------------"
+    
+    num_seq=0
+    total_leg_seq=0
+    leg_long_seq=0
+    leg_sho_seq=0
+    avr_seq_leg=0
+    GC_cont=0
+
+    echo "Number of sequences: $num_seq"
+    echo "Total length of sequences: $total_leg_seq"
+    echo "Length of the longest sequence: $leg_long_seq"
+    echo "Length of the shortest sequence: $leg_sho_seq"
+    echo "Average sequence length: $avr_seq_leg"
+    echo "GC Content (%): $GC_cont "
+    echo ""
+done 
---
+++ EXC-003/fasta-file-processor.sh
@@ -0,0 +1,9 @@
+# EXC-003 
+# write a shell script that accepts every FASTA file and gives a report with the following variables
+# Number of sequences:
+# Total length of sequences
+# Length of longest sequence: 
+# Length of shortest sequence: 
+# Average sequence length: 
+# GC Content ( in %): 
+# 
