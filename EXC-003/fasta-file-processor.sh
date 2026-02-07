a915930b8afb631be4dd25a9c479aeca9067841a
Talina Helms <talina.helms@ck-helms.de>
2026-02-07 08:33:06 +0100

    my changes

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,9 +1,42 @@
-# EXC-003 
-# write a shell script that accepts every FASTA file and gives a report with the following variables
-# Number of sequences:
-# Total length of sequences
-# Length of longest sequence: 
-# Length of shortest sequence: 
-# Average sequence length: 
-# GC Content ( in %): 
-# 
+ for f in $@
+do 
+    echo "FASTA File_Statistic" # Header 
+    echo "----------------------"
+
+    num_seq=$(grep '>' $f | wc -l ) 
+        echo "Number of sequences: $num_seq"
+   
+    total_leg_seq=$(awk '!/>/{total += gsub(/[AaTtGgCc]/, "")} END {print total}' $f)
+        echo "Total length of sequences: $total_leg_seq"
+
+    length_seq_max=$(awk '/^>/ {
+                                if (seqlen > max) max = seqlen
+                                                        seqlen = 0
+                                                        next }
+                                            {seqlen += length($0)}
+                                END {if (seqlen > max) max = seqlen
+                                                    print max} ' $f )
+
+    leg_long_seq=$(echo "$length_seq_max" | sort -n | tail -n 1)
+        echo "Length of the longest sequence: $leg_long_seq"
+
+lenght_seq_min=$(awk 'BEGIN {min = -1}
+                        /^>/ {if (seqlen > 0) {if (min < 0 || seqlen < min) 
+                                    min = seqlen}
+                                    seqlen = 0
+                                    next}
+                                {seqlen += length($0)}
+                     END {if (seqlen > 0 && (min < 0 || seqlen < min))
+                                {min = seqlen} print min} ' $f)
+
+leg_sho_seq=$(echo "$lenght_seq_min")
+        echo "Length of the shortest sequence: $leg_sho_seq"
+#Here needs to be a If-Condition so it runs even without a good FASTA file
+        echo "Average sequence length: $(echo " scale=3; $total_leg_seq/$num_seq" | bc) "
+
+    GC_cont=$(awk '!/>/{gc_count += gsub(/[GgCc]/, "")} END {print gc_count}' $f)
+         echo "GC Content (%):$(echo "scale=3; $GC_cont *100 / $total_leg_seq" | bc) "
+         
+        echo ""
+done 
+
