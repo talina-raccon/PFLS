b95f40f0c904302336ceb3b1137be674ddb1fc41
Talina Helms <talina.helms@ck-helms.de>
2026-02-09 10:38:22 +0100

    my changes

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,6 +1,6 @@
  for f in $@
 do 
-    echo "FASTA File Statistic" # Header 
+    echo "FASTA File Statistics" # Header 
     echo "----------------------"
 
     num_seq=$(grep '>' $f | wc -l | awk '{print $1}' ) 
