1ab25380faf959ebf757ce0285e2fe895d0e9c0a
Talina Helms <talina.helms@ck-helms.de>
2026-02-09 10:30:29 +0100

    my changes

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,9 +1,9 @@
  for f in $@
 do 
-    echo "FASTA File_Statistic" # Header 
+    echo "FASTA File Statistic" # Header 
     echo "----------------------"
 
-    num_seq=$(grep '>' $f | wc -l ) 
+    num_seq=$(grep '>' $f | wc -l | awk '{print $1} ' ) 
         echo "Number of sequences: $num_seq"
    
     total_leg_seq=$(awk '!/>/{total += gsub(/[AaTtGgCc]/, "")} END {print total}' $f)
@@ -35,7 +35,7 @@ leg_sho_seq=$(echo "$lenght_seq_min")
         echo "Average sequence length: $(echo " scale=3; $total_leg_seq/$num_seq" | bc) "
 
     GC_cont=$(awk '!/>/{gc_count += gsub(/[GgCc]/, "")} END {print gc_count}' $f)
-         echo "GC Content (%):$(echo "scale=3; $GC_cont *100 / $total_leg_seq" | bc) "
+         echo "GC Content (%): $(echo "scale=3; $GC_cont *100 / $total_leg_seq" | bc) "
          
         echo ""
 done 
